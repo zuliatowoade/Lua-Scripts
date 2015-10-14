24716af9f786f4803ada3a54b11b4d104030edae
zuliatowoade <zowoade@smith.edu>
2015-10-14 16:46:48 -0400

    Create all.lua

---
+++ Lua-Scripts/all.lua
@@ -0,0 +1,26 @@
+local schema = ARGV[1]
+local query = ARGV[2]     #a dictionary of fields
+
+data = {}
+for  field in query:
+    local idxkey =  schema["name"] .. ":" .. field .. ":idx"
+    local value = query[field]
+    table.insert(data, redis.call("ZRANGEBYLEXASYNC", idxkey, ("'[" .. value .. ":'"), ("'[" .. value .. ":'\xff")  0, id))
+end 
+  
+local valueIdToDocKey = function (dataItem)
+    dataItem = split(dataItem, ":")
+    id = table.remove(dataItem)
+    local docKey = schema["name"] .. ":" .. id
+    return dockey
+    
+end
+
+
+keys = {}
+for item in data:
+     table.insert(keys, valueIdToDockey(item))
+     
+     
+local docs = redis.call('MGET', keys)
+return cjson.encode(docs)
