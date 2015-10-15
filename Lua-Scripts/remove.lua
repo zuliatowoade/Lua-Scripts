63930a7d97444173623911f4e6450dd541a95d3b
zuliatowoade <zowoade@smith.edu>
2015-10-15 08:21:52 -0400

    Update remove.lua

--- Lua-Scripts/remove.lua
+++ Lua-Scripts/remove.lua
@@ -1,9 +1,8 @@
 local schema = cjson.decode(ARGV[1])
 local doc    = cjson.decode(ARGV[2])
+local id = doc["id"]
 local docKey = schema["name"] .. ":" .. id
 local idIdx = schema["name"] .. ":" .. "ids"
-local id = doc["id"]
-
 local docCount = redis.call("DEL", dockey)
 local idxCount =  redis.call("ZREM", idIdx, id)
   
